a720ef432cc77247dcc73068382c57df9a24d3d8
Patrick Ryan <patrick@scottsmarketplace.com>
2015-02-22 19:49:51 -0700

    Added CBV Mixins PR #92

--- test_app/views.py
+++ test_app/views.py
@@ -2,9 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response, render
 from django.template import Context, RequestContext
 from django.template.loader import render_to_string
+from django.views.generic import View
 
 from waffle import flag_is_active
 from waffle.decorators import waffle_flag, waffle_switch
+from waffle.mixins import WaffleFlagMixin, WaffleSampleMixin, WaffleSwitchMixin
 
 
 def flag_in_view(request):
@@ -49,3 +51,32 @@ def flagged_view(request):
 @waffle_flag('!foo')
 def flagged_off_view(request):
     return HttpResponse('foo')
+
+
+class BaseWaffleView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('foo')
+
+
+class FlagView(WaffleFlagMixin, BaseWaffleView):
+    waffle_flag = 'foo'
+
+
+class FlagOffView(WaffleFlagMixin, BaseWaffleView):
+    waffle_flag = '!foo'
+
+
+class SampleView(WaffleSampleMixin, BaseWaffleView):
+    waffle_sample = 'foo'
+
+
+class SampleOffView(WaffleSampleMixin, BaseWaffleView):
+    waffle_sample = '!foo'
+
+
+class SwitchView(WaffleSwitchMixin, BaseWaffleView):
+    waffle_switch = 'foo'
+
+
+class SwitchOffView(WaffleSwitchMixin, BaseWaffleView):
+    waffle_switch = '!foo'
